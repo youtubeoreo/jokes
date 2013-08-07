15fcbb2e2a8eaac31b6e068dfdad6d53105e1216
youtubeoreo <shrek2357@gmail.com>
2013-08-07 09:48:33 -0700

    Create jokes.ino

---
+++ jokes.ino
@@ -0,0 +1,35 @@
+Q: Do you know how an electrician tells if he's working with AC or DC power?
+A: If it's AC, his teeth chatter when he grabs the conductors. If it's DC, they just clamp together.
+
+Q: What did the light bulb say to the generator?
+A: "I really get a charge out of you!"
+
+What Thomas Edison's Mother might have said to her son:
+"Of course I'm proud that you invented the electric light bulb. Now turn it off and get to bed!"
+
+Sign on on the side of the electrician’s van – “Let Us Get Rid of Your Shorts”.
+
+At the electric company: "We would be delighted if you send in your bill. However, if you don't, you will be."
+
+Q: What is an electrician's favorite ice cream flavor?
+A: Shock-o-lot!
+
+Q: What do you call a carpenter working in a electrical panel?
+A: Dead!
+
+Q: How do you know when a union electrician is dead?
+A: The doughnut rolls out of his hand.
+
+Two atoms were walking down the street one day, when one of them exclaimed, "Oh no - I've lost an electron!" "Are you sure?" the other one asked. "Yes," replied the first one, "I'm positive."
+
+Q: Why are electricians always up to date?
+A: Because they are "Current specialists".
+
+Q: How do you pick out a dead battery from a pile of good ones? 
+A: It's got no spark!
+
+Q: What would you call a power failure?
+A: A current event.
+
+Who was the first electric detective?
+Sherlock Ohms
