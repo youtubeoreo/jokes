37464fb4b3cd5767d9f0491035811583a244ead0
youtubeoreo <shrek2357@gmail.com>
2013-08-07 17:11:41 -0700

    Update jokes.ino

--- jokes.ino
+++ jokes.ino
@@ -33,3 +33,28 @@ A: A current event.
 
 Who was the first electric detective?
 Sherlock Ohms
+
+Q:How many different tyoes of people are in the world?
+A:10,those who don't understand binary and those who do.
+(
+1.1
+2. 10 
+3. 11 
+4. 100 
+5. 101 
+6. 110 
+7. 111 
+8. 1000 
+9. 1001 
+10. 1010 
+11. 1011 
+12. 1100 
+13. 1101 
+14. 1110 
+15. 1111 
+16. 10000 
+17. 10001 
+18. 10010 
+19. 10011 
+20. 10100
+)
